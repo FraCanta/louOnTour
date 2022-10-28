13e04f16a4ddd4c9744befad90733a518299ea66
FraCanta <you@example.comfcantale14@gmail.com>
2022-10-28 13:35:30 +0200

    hero and nav

--- components/button/button.js
+++ components/button/button.js
@@ -3,7 +3,7 @@ import { Icon } from "@iconify/react";
 
 function Cta({ children }) {
   return (
-    <button className="border-2 px-2 fxl:px-4 2xl:py-[1.2rem] fxl:py-[1.5rem] text-white 2xl:text-[1.2rem] fxl:text-[1.5rem] font-bold uppercase bg-[#E3494D] border-[#E3494D] rounded-2xl flex justify-evenly min-w-[220px]">
+    <button className="border-2 px-4 fxl:px-4 2xl:py-[1.2rem] fxl:py-[12rem] text-white 2xl:text-[1.2rem] fxl:text-[1.5rem] font-bold uppercase bg-[#E3494D] border-[#E3494D] rounded-2xl flex justify-evenly 2xl:min-w-[220px]">
       {children} <Icon icon="bi:arrow-right" />
     </button>
   );
--- components/layout/hero.js
+++ components/layout/hero.js
@@ -5,10 +5,10 @@ const Hero = () => {
   return (
     <div className="hero_container leading-none w-full">
       <section className="text-center h-[calc(100vh_-_98px)] flex flex-col items-center justify-center hero">
-        <h2 className="text-[1.8rem] 2xl:text-[2.5rem] fxl:text-[4rem] font-medium text-[#D93280] mb-4 mt-10">
+        <h2 className="text-[1.2rem] 2xl:text-[2.5rem] fxl:text-[4rem] font-medium text-[#D93280] mb-4 mt-10">
           Tuscan Experience
         </h2>
-        <h1 className="text-[6.5rem] 2xl:text-[12rem] fxl:text-[15.625rem] font-bold tracking-[-0.020em] text-white mb-8 mt-5">
+        <h1 className="text-[4.5rem] 2xl:text-[12rem] fxl:text-[15.625rem] font-bold tracking-[-0.020em] text-white mb-8 mt-5">
           Lou On Tour
         </h1>
 
