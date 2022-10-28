9778d24f1c5c7318336628fcf246f8e91ba81359
FraCanta <you@example.comfcantale14@gmail.com>
2022-10-28 14:55:52 +0200

    hero and nav

--- components/layout/menu_mobile.js
+++ components/layout/menu_mobile.js
@@ -1,75 +1,199 @@
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Lou from "../../public/assets/lou.webp";
+import gsap from "gsap";
+import { Power1, Power2 } from "gsap";
+
 function Menu_mobile() {
+  const [mobileMenu, setMobileMenu] = useState({
+    isOpen: false,
+    fastClose: false,
+  });
+
+  const handleBurgerClick = () => {
+    setMobileMenu((prevData) => {
+      return { ...prevData, isOpen: !prevData.isOpen };
+    });
+  };
+  const handleBurgerClose = () => {
+    setMobileMenu({ isOpen: false, fastClose: true });
+  };
+
+  useEffect(() => {
+    var tl = gsap.timeline({ defaults: { ease: Power1.easeOut } });
+    if (mobileMenu.isOpen) {
+      {
+        /*  quando è aperto */
+      }
+
+      tl.set(".menu_mobile_content", {
+        width: 0,
+        opacity: 0,
+      });
+      tl.set([".bar-1", "bar-3"], {
+        width: "20px",
+        rotation: 0,
+        y: 0,
+      });
+      tl.to(".menu-hamburger", {
+        duration: 0.2,
+        rotation: 90,
+      });
+      tl.to(".bar-2", {
+        width: "5px",
+        duration: 0.04,
+      });
+
+      tl.to(".menu_mobile_content", {
+        duration: 0.5,
+        width: "100vw",
+        opacity: 1,
+        // bottom: 0,
+        ease: Power1.inOut,
+      });
+      tl.to(".bar-1", {
+        duration: 0.2,
+        y: 8,
+        rotation: 45,
+        ease: Power2.easeInOut,
+      });
+
+      tl.to(".bar-3", {
+        duration: 0.08,
+        rotation: -45,
+        y: -10,
+        ease: Power2.easeInOut,
+      });
+      tl.to(".bar-2", {
+        duration: 0.2,
+        autoAlpha: 0,
+        x: "100vh",
+        width: "55px",
+        ease: Power2.easeInOut,
+      });
+      tl.to(".menu_mobile_item li a", {
+        duration: 0.08,
+        opacity: 1,
+        stagger: {
+          each: 0.5,
+        },
+      });
+
+      tl.to(
+        ".btn_mobile_contact",
+        {
+          duration: 0.4,
+          opacity: 1,
+        },
+        "+=0.5"
+      );
+      tl.to(".social_mobile a", {
+        duration: 0.3,
+        opacity: 1,
+        stagger: {
+          each: 0.2,
+        },
+      });
+    } else if (!mobileMenu.isOpen) {
+      {
+        /* quando è chiuso */
+      }
+      {
+        mobileMenu.fastClose &&
+          tl.to(".menu_mobile_content", {
+            duration: 0.6,
+            width: "0%",
+            opacity: 0,
+            ease: Power1.inOut,
+          });
+      }
+      tl.to(".bar-1", {
+        duration: 0.2,
+        y: 0,
+        rotation: 0,
+        ease: Power2.easeInOut,
+      });
+      tl.to(".bar-3", {
+        duration: 0.2,
+        rotation: 0,
+        y: 0,
+        ease: Power2.easeInOut,
+      });
+      tl.to(".bar-2", {
+        duration: 0.5,
+        autoAlpha: 1,
+        ease: Power2.easeInOut,
+        rotation: 0,
+        x: 0,
+        width: "30px",
+      });
+      tl.to(".menu-hamburger", {
+        duration: 0.2,
+        rotation: 0,
+      });
+      tl.to(".menu_mobile_content", {
+        duration: 0.6,
+        width: "0%",
+        opacity: 0,
+        ease: Power1.inOut,
+      });
+      tl.to(".menu_mobile_item li a", {
+        duration: 0.3,
+        opacity: 0,
+        stagger: 0.1,
+      });
+
+      {
+        mobileMenu.fastClose &&
+          setMobileMenu({ isOpen: false, fastClose: false });
+      }
+    }
+  }); // eslint-disable-next-line no-console
+
   return (
     <div className="block xl:hidden sticky top-0 right-0 z-10  flex">
-      <div className="flex justify-between h-[70px] items-center w-full  px-8 md:px-12 py-4">
+      <div className="header_mobile  flex justify-between h-[70px] items-center w-full  px-8 md:px-12 py-4">
         <div>
           <Link href="/">
             <Image src={Lou} alt="Lou Logo" width={50} height={50} />
           </Link>
         </div>
 
-        <div className="menu-hamburger">
+        <div className="menu-hamburger" onClick={handleBurgerClick}>
           <div className="menu-hamburger-line bar-1"></div>
           <div className="menu-hamburger-line bar-2"></div>
           <div className="menu-hamburger-line bar-3"></div>
         </div>
       </div>
-      {/* <div className="menu_mobile_content dark:bg-black">
+      <div className="menu_mobile_content ">
         <div className="menu_mobile_body">
           <div className="menu-nav menu_mobile_item">
-            <ul className="w-4/5  ">
+            <ul onClick={handleBurgerClose} className="w-4/5  ">
               <li>
-                <Link href="/" className="dark:text-white">
-                    Home
+                <Link href="/" className="text-black">
+                  Home
                 </Link>
               </li>
               <li>
-                <Link href="/about" className="dark:text-white">
-                  {translation?.[locale]?.about}
+                <Link href="/about" className="text-black">
+                  Tours
                 </Link>
               </li>
               <li>
-                <Link href="/how" className="dark:text-white">
-                  {translation?.[locale]?.how}
+                <Link href="/how" className="text-black">
+                  Chi sono
                 </Link>
               </li>
               <li>
-                <Link href="/pricing" className="dark:text-white">
-                  {translation?.[locale]?.pricing}
+                <Link href="/pricing" className="text-black">
+                  Contatti
                 </Link>
               </li>
             </ul>
           </div>
-          <div className="btn_mobile_contact w-4/5">
-            <button className="btn">
-              <Link href="/contact">{translation?.[locale]?.contact}</Link>
-            </button>
-            <div className="btn_shadow"></div>
-          </div>
-          <div className="social_mobile w-4/5">
-            <a
-              href="https://www.facebook.com/ubiquitywebcreativelab"
-              target="_blank"
-              rel="noReferrer"
-              className="dark:text-white"
-            >
-              Fb
-            </a>
-            <a
-              href="https://www.linkedin.com/company/ubiquity-web-creative-lab/"
-              target="_blank"
-              rel="noReferrer"
-              className="dark:text-white"
-            >
-              Ln
-            </a>
-          </div>
         </div>
-      </div> */}
+      </div>
     </div>
   );
 }
