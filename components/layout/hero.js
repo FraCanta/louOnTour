2f7685edc6a136f172257fb541986b1135a3dd6a
FraCanta <you@example.comfcantale14@gmail.com>
2022-10-28 18:06:21 +0200

    menu

--- components/layout/hero.js
+++ components/layout/hero.js
@@ -1,24 +1,35 @@
 import React from "react";
 import Cta from "../button/button";
+import { Icon } from "@iconify/react";
 
 const Hero = () => {
   return (
-    <div className="hero_container leading-none w-full">
-      <section className="text-center h-[calc(100vh_-_80px)] xl:h-[calc(100vh_-_100px)] flex flex-col items-center justify-center hero">
-        <h2 className="text-[1.2rem] md:text-[2rem] 2xl:text-[2.5rem] lg:text-[2.5rem] xl:text-[3.5rem] fxl:text-[4rem] font-semibold text-[#D93280] mb-4 mt-10 rotated:mb-0 rotated:mt-0 rotated:text-[1.5rem]">
-          Tuscan Experience
-        </h2>
-        <h1 className="text-[4.5rem] md:text-[8rem] 2xl:text-[12rem] lg:text-[8rem] xl:text-[10rem] fxl:text-[15.625rem] font-bold tracking-[-0.020em] text-white mb-5 md:mb-8 mt-5 rotated:text-[5rem] rotated:mb-5">
-          Lou On Tour
-        </h1>
+    <>
+      <div className="hero_container leading-none w-full">
+        <section className="text-center h-[calc(100vh_-_80px)] xl:h-[calc(100vh_-_100px)] flex flex-col items-center justify-center hero">
+          <h2 className="text-[1.2rem] md:text-[2rem] 2xl:text-[2.5rem] lg:text-[2.5rem] xl:text-[3.5rem] fxl:text-[4rem] font-semibold text-[#D93280] mb-4 mt-10 rotated:mb-0 rotated:mt-0 rotated:text-[1.5rem]">
+            Tuscan Experience
+          </h2>
+          <h1 className="text-[4.5rem] md:text-[8rem] 2xl:text-[12rem] lg:text-[8rem] xl:text-[10rem] fxl:text-[15.625rem] font-bold tracking-[-0.020em] text-white mb-5 md:mb-8 mt-5 rotated:text-[5rem] rotated:mb-5">
+            Lou On Tour
+          </h1>
 
-        <p className="w-4/5 2xl:w-3/4 mx-auto text-white text-[0.86rem] md:text-[1.2rem] 2xl:text-[1.4rem] lg:text-[1.2rem] xl:text-[1.5rem] fxl:text-[2.5rem] leading-tight mb-5">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo
-          necessitatibus ipsam, assumenda, modi.
-        </p>
-        <Cta>get started</Cta>
-      </section>
-    </div>
+          <p className="w-4/5 2xl:w-3/4 mx-auto text-white text-[0.86rem] md:text-[1.2rem] 2xl:text-[1.4rem] lg:text-[1.2rem] xl:text-[1.5rem] fxl:text-[2.5rem] leading-tight mb-5">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo
+            necessitatibus ipsam, assumenda, modi.
+          </p>
+          <Cta>get started</Cta>
+        </section>
+      </div>
+      <div className="shape">
+        <Icon
+          icon="carbon:arrow-down"
+          color="#0d0d0d"
+          width="50"
+          className="arrow_icon"
+        />
+      </div>
+    </>
   );
 };
 
