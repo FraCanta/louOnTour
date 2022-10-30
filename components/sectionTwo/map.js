640ce50a00428f5e4ab03bd2c842ad67360ea5b6
FraCanta <you@example.comfcantale14@gmail.com>
2022-10-30 12:47:06 +0100

    sections

--- components/sectionTwo/map.js
+++ components/sectionTwo/map.js
@@ -5,11 +5,11 @@ import Toscana from "../../public/assets/Toscana.png";
 const Map = () => {
   return (
     <div className="h-[50vh]  pt-20 container mx-auto w-4/5">
-      <div className="grid gap-14 md:gap-14 xl:gap-28 grid-cols-1 md:grid-cols-2 justify-items-center content-center pt-60 pb-12">
+      <div className="grid gap-14 md:gap-14 xl:gap-28 grid-cols-1 md:grid-cols-2 justify-items-center content-center pt-12 pb-12">
         <div>
           <h4 className="text-[#5D68A6]">Tours</h4>
-          <h2 className="text-4xl md:text-[64px] font-medium mt-2 leading-10">
-            Welcome to Lou On Tour
+          <h2 className="text-4xl md:text-[64px] font-medium mt-2 leading-[4.3rem]">
+            Disegnamo insieme il Tou Tour
           </h2>
           <p className="text-base sm:text-lg  mt-4 sm:mt-16 mb-8 text-[#2d2d2d] ">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
@@ -26,7 +26,7 @@ const Map = () => {
           alt="brand"
           width={600}
           height={600}
-          className="brand-img rounded-lg"
+          //   className="brand-img rounded-lg"
         />
       </div>
     </div>
