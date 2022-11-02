2687886f71df2710e8549d6280a711edc92ae44b
FraCanta <you@example.comfcantale14@gmail.com>
2022-11-02 15:00:15 +0100

    mission

--- components/sectionOne/mission.js
+++ components/sectionOne/mission.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Mission = () => {
   return (
-    <div className="min-h-[60vh] text-center pt-20">
+    <div className="min-h-[60vh] text-center pt-2 2xl:pt-20">
       <div className="container w-4/5 md:w-4/5 mx-auto pt-12 md:pt-10">
         <div className="mx-auto relative z-1">
           <div>
