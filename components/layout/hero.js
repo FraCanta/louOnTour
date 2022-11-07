809eee990d4d42053014d38bee67098aed843e08
FraCanta <you@example.comfcantale14@gmail.com>
2022-11-07 15:36:27 +0100

    hero

--- components/layout/hero.js
+++ components/layout/hero.js
@@ -31,9 +31,11 @@ const Hero = () => {
           <h1 className="text-[4.5rem] md:text-[8rem] 2xl:text-[12rem] lg:text-[8rem] xl:text-[10rem] fxl:text-[15.625rem] font-bold tracking-[-0.020em] text-white mb-5 md:mb-0 mt-5 rotated:text-[5rem] rotated:mb-5">
             Lou On Tour
           </h1>
-          <p className="w-4/5 2xl:w-3/4 mx-auto text-white text-[0.86rem] md:text-[1.2rem] 2xl:text-[1.4rem] lg:text-[1.2rem] xl:text-[1.5rem] fxl:text-[2.5rem] leading-tight mb-5">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo
-            necessitatibus ipsam, assumenda, modi.
+          <p className="w-4/5 2xl:w-3/4 mx-auto text-white text-[0.96rem] md:text-[1.2rem] 2xl:text-[1.5rem] lg:text-[1.2rem] xl:text-[1.5rem] fxl:text-[2.5rem] leading-7 mb-5 font-bold">
+            Sono Luisa Quaglia, Guida Turistica abilitata in italiano e in
+            inglese. Propongo tour guidati a Firenze, Volterra, Siena e la sua
+            provincia. Sono a tua disposizione per disegnare il tuo tour su
+            misura, secondo i tuoi gusti e le tue esigenze.
           </p>
           <Cta>get started</Cta>
         </div>
