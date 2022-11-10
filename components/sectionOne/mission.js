11cd3c97c32a98d2ee9d9d9e483ab634ecd3980b
FraCanta <you@example.comfcantale14@gmail.com>
2022-11-10 11:42:16 +0100

    about page

--- components/sectionOne/mission.js
+++ components/sectionOne/mission.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Mission = () => {
   return (
-    <div className="min-h-[100vh] text-center pt-2 2xl:pt-0 flex items-center justify-center">
+    <div className="min-h-[100vh] text-left 2xl:text-center pt-2 2xl:pt-0 flex items-center justify-center">
       <div className="container w-4/5 md:w-4/5 mx-auto pt-12 md:pt-2 xl:h-full ">
         <div className="mx-auto relative z-1">
           <div>
--- pages/[id]/[id].js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const [id] = () => {
-  return <div>[id]</div>;
-};
-
-export default [id];
