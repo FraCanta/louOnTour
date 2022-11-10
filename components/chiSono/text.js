e78f8ee1f6f90545ca16ae6c71d640113343e306
FraCanta <you@example.comfcantale14@gmail.com>
2022-11-10 11:44:30 +0100

    about page

--- components/chiSono/text.js
+++ components/chiSono/text.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Text = () => {
   return (
-    <div className="min-h-[70vh] text-center pt-10 2xl:pt-10 flex items-center justify-center">
+    <div className="min-h-[70vh] text-left 2xl:text-center pt-10 2xl:pt-10 flex items-center justify-center">
       <div className="container w-4/5 md:w-4/5 mx-auto pt-12 md:pt-2 xl:h-full ">
         <div className="mx-auto relative z-1">
           <div>
@@ -10,19 +10,13 @@ const Text = () => {
               Ho fatto della mia passione il mio lavoro
             </h3>
             <p className="text-base sm:text-lg 2xl:text-xl fxl:text-2xl   mt-4 sm:mt-16 mb-8 text-[#515151] leading-6 2xl:leading-9 2xl:w-[80% mx-auto">
-              Se sei approdato su questo sito molto probabilmente stai cercando
-              un’esperienza da vivere qui in Toscana assieme al tuo partner, ai
-              tuoi amici o alla tua famiglia. Allora sei nel sito giusto perché
-              “ho un’offerta che non potrai rifiutare!”. Scherzi a parte, io
-              sono Luisa, guida turistica e archeologa e sono qui per guidarti
-              alla scoperta di questa splendida regione italiana. Ma questo è
-              anche il sito giusto per chi vuole godersi appieno il suo viaggio,
-              che cerca l’essenza del luogo, attraverso uno scorcio
-              caratteristico o un’opera d’arte; attraverso la storia di un
-              personaggio famoso, o le mani esperte di un orgoglioso artigiano
-              locale; per non parlare della scoperta di nuovi sapori e di
-              antiche tradizioni. E tutto al tuo passo! Che ne pensi, fa al caso
-              tuo? Allora dai una sbirciata ai miei tour.
+              Queste ultime esperienze mi hanno fatto apprezzare
+              la divulgazione, quindi ho deciso di diventare una guida
+              turistica: la scelta migliore della mia vita! Amo incontrare
+              persone nuove e condividere con loro la mia passione per la storia
+              e l’arte. Incontro persone che vengono da tutta Italia e
+              dall’estero: un’occasione questa per me, per conoscere altre
+              storie e culture, una sorta di viaggio senza fine, fantastico!
             </p>
           </div>
         </div>
