55929d11e88ab4308611973712b14bcc1bc57dd5
FraCanta <you@example.comfcantale14@gmail.com>
2022-11-18 12:07:37 +0100

    social img

--- components/sectionFour/insta.js
+++ components/sectionFour/insta.js
@@ -46,8 +46,8 @@ const Insta = () => {
           </div>
         </div>
         <div className=" 2xl:p-0 w-11/12 lg:w-4/5 flex flex-col items-start justify-center">
-          <h4 className="text-[#FE6847]">Social</h4>
-          <h2 className="text-4xl md:text-[64px] font-medium md:leading-none lg:leading-none text-[#232F37]">
+          <h4 className="text-[#FE6847] text-xl 3xl:text-4xl">Social</h4>
+          <h2 className="text-4xl md:text-[64px] 3xl:text-[100px] font-medium md:leading-none lg:leading-none text-[#232F37]">
             Lou On Tour Instagram
           </h2>
           <p className="text-base sm:text-lg 2xl:text-xl fxl:text-2xl  mt-4 sm:mt-10 mb-8 text-[#515151] ">
