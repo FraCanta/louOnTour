60f2552ede852b819c89fbd3335ac1a676ed2ec2
FraCanta <you@example.comfcantale14@gmail.com>
2022-11-21 14:04:42 +0100

    slider

--- components/UI/correlati.js
+++ components/UI/correlati.js
@@ -6,21 +6,21 @@ import Link from "next/link";
 const Correlati = ({ city, others }) => {
   return (
     <>
+      <div className="container w-11/12 xl:w-4/5 mx-auto">
+        <h3 className="text-3xl md:text-[40px] font-medium mt-2 leading-10 text-[#232F37] lg:text-center pb-8 ">
+          Ecco per te altre proposte
+        </h3>
+      </div>
       <div className=" min-h-[38vh] lg:min-h-[40vh] 3xl:min-h-[80vh]  w-full 2xl:p-8 ">
-        <div className="container w-11/12 xl:w-4/5 mx-auto">
-          <h3 className="text-3xl md:text-[40px] font-medium mt-2 leading-10 text-[#232F37] lg:text-center pb-8 ">
-            Ecco per te altre proposte
-          </h3>
-        </div>
         <div
-          className={`hidden xl:flex container mx-auto w-full xl:w-4/5 text-[1.5rem] xl:text-5xl mb-10 text-white hand-pointer overflow-hidden `}
+          className={`container mx-auto w-full xl:w-4/5 text-[1.5rem] xl:text-5xl mb-10 text-white overflow-hidden hidden xl:flex `}
         >
           {others
             ?.filter((el) => el?.title !== city?.titleImg)
             .map((el, i) => (
               <>
                 <div className={`  w-[25%]  relative aspect-square `} key={i}>
-                  <Link href={el?.link}>
+                  <Link href={el?.link} className=" hand-pointer">
                     <Image
                       src={el?.img}
                       alt={el?.titleImg}
@@ -55,6 +55,11 @@ const Correlati = ({ city, others }) => {
                       className="rounded-box  h-full w-full absolute"
                     />
                   </Link>
+                  <div
+                    className={`absolute bottom-[1rem]  left-[1rem] bgText  z-20 text-base xl:text-2xl w-3/5 text-white`}
+                  >
+                    <p className="leading-4 text-xl">{el?.title}</p>
+                  </div>
                 </div>
               </>
             ))}
