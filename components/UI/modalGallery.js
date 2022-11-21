bd8026a2649699c0cd7efd6e6a98ad22500cfe52
FraCanta <you@example.comfcantale14@gmail.com>
2022-11-21 16:53:45 +0100

    correlati

--- components/UI/modalGallery.js
+++ components/UI/modalGallery.js
@@ -6,22 +6,21 @@ const ModalCorrelati = ({ handleModal, showModal }) => {
   return (
     <div
       className={`flex items-center justify-center w-screen h-screen fixed top-0 left-0 bottom-0 right-0 z-20 bg-black bg-opacity-20 overlay ${showModal?.isOpen} `}
-      onClick={() => handleModal("")}
+      onClick={() => handleModal(showModal?.img)}
     >
       <div
         className={`w-full h-full rounded-[10px] text-black flex flex-col justify-center  items-center border-none   bg-clip-padding text-current bg-modal ${showModal?.scale} bold`}
       >
-        <div className=" ">
-          <Image
-            src={showModal?.img}
-            alt=""
-            // objectFit="contain"
-            // fill
-            // priority
-            width={700}
-            height={700}
-          />
-        </div>
+        <Image
+          src={showModal?.img}
+          alt=""
+          // objectFit="contain"
+          // fill
+          priority="true"
+          width={700}
+          height={700}
+          className="max-h-[80%] max-w-[80%]"
+        />
       </div>
     </div>
   );
