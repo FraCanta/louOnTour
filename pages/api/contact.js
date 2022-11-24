83c1db3358b551bfae0359d134aac6417ff739d1
FraCanta <you@example.comfcantale14@gmail.com>
2022-11-24 15:51:29 +0100

    form

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -3,22 +3,22 @@ import nodemailer from "nodemailer";
 export default async function mailer(req, res) {
   const { name, surname, email, phone, project_type, message } = req.body;
   const transporter = nodemailer.createTransport({
-    host: "out.email.it",
+    host: "smtp.mail.yahoo.it",
     port: 465,
     secure: true,
     auth: {
       //   user: process.env.SMTP_USER, se vogliamo inserire le credenziali su env
       //   pass: process.env.SMTP_PASSWORD,
-      user: "automotive_demo",
-      pass: "Mentadent@16",
+      user: "rachele164",
+      pass: "umpalumpa",
     },
   });
 
   try {
     await transporter.sendMail({
-      from: "automotive_demo@email.it",
+      from: "rachele164@yahoo.it",
       to: ["eddironz@hotmail.com", "fcantale14@gmail.com"],
-      subject: `UbiquityMessage da ${name} ${surname} `,
+      subject: `You've got a LouMessage da ${name} ${surname} `,
       html: `<p>Hai una richiesta di informazioni da ${name} ${surname}</p><br>
         <p><strong>Email: </strong> ${email}</p><br>
         <p><strong>Tipo di progetto: </strong> ${project_type}</p><br>
