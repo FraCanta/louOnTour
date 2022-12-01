45c0cc659618a5975cd06962a69e8ce8b60dda81
Edmondo-diRonza <eddironz@hotmail.com>
2022-12-01 17:50:14 +0100

    security patches

--- next.config.js
+++ next.config.js
@@ -5,6 +5,33 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 };
+const ContentSecurityPolicy = `
+  default-src 'self' https://api.iconify.design/ https://fonts.googleapis.com https://fonts.gstatic.com https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.css;
+  script-src 'self' https://storage.googleapis.com 'unsafe-inline' 'unsafe-eval';
+  child-src 'self' http://localhost:3000/ https://lou-eight.vercel.app/;
+  style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.css data:;
+  font-src 'self' https://fonts.googleapis.com https://fonts.gstatic.com https://cdnjs.cloudflare.com/ 'unsafe-inline' data:;
+  img-src 'self' data: blob:;
+  
+`;
+const securityHeaders = [
+  {
+    key: "X-XSS-Protection",
+    value: "1; mode=block",
+  },
+  {
+    key: "X-Frame-Options",
+    value: "SAMEORIGIN",
+  },
+  {
+    key: "X-Content-Type-Options",
+    value: "nosniff",
+  },
+  {
+    key: "Content-Security-Policy",
+    value: ContentSecurityPolicy.replace(/\s{2,}/g, " ").trim(),
+  },
+];
 
 module.exports = withPlugins([
   nextConfig,
@@ -14,6 +41,17 @@ module.exports = withPlugins([
       locales: ["it", "en"],
       defaultLocale: "it",
       localeDetection: true,
+    },
+  },
+  {
+    async headers() {
+      return [
+        {
+          // Apply these headers to all routes in your application.
+          source: "/:path*",
+          headers: securityHeaders,
+        },
+      ];
     },
   },
 ]);
