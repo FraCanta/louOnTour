b6e887bdeb0f33e1151b9237c54f32827285d7e8
FraCanta <you@example.comfcantale14@gmail.com>
2022-12-13 17:41:48 +0100

    blog section

--- components/blogSection/blogSection.js
+++ components/blogSection/blogSection.js
@@ -4,36 +4,36 @@ import { Icon } from "@iconify/react";
 import Post from "../Post/post";
 import Cta from "../button/button";
 import { useRouter } from "next/router";
+import PostSection from "./postSection";
 
 const BlogSection = ({ post, featuredMedia, tags }) => {
-  // console.log(post);
-
   const jsxPosts = post.map((p, i) => {
     const featuredMedia = p?.["_embedded"]?.["wp:featuredmedia"][0];
-    return <Post post={p} featuredMedia={featuredMedia} key={p.id} />;
+    return <PostSection post={p} featuredMedia={featuredMedia} key={p.id} />;
   });
   return (
-    <div className="hero min-h-[38vh] lg:min-h-[68vh] 3xl:min-h-[80vh]  w-full bg-[#232F37]  text-white flex items-center  overflow-x-hidden">
-      <div className="grid gap-14 md:gap-14 xl:gap-18 grid-cols-1  justify-items-center content-center w-11/12 2xl:w-4/5 mx-auto h-full pt-10">
-        <div>
-          <h4 className="text-[#FE6847] text-xl 3xl:text-4xl text-bold">
+    <div className="hero min-h-[38vh] lg:min-h-[68vh] 3xl:min-h-[80vh]  w-full bg-[#232F37]  text-white flex items-center  overflow-x-hidden py-20 ">
+      <div className="grid  md:gap-14 xl:gap-18 grid-cols-1  justify-items-center content-center w-11/12 2xl:w-4/5 mx-auto h-full ">
+        <div className="text-center">
+          <h4 className="text-[#232F37] text-xl 3xl:text-4xl font-bold">
             Blog
           </h4>
           <h2 className="text-4xl md:text-[64px] 3xl:text-[100px] font-medium  md:leading-none lg:leading-none mb-10">
             Ultime News
           </h2>
-          <p className="text-base sm:text-lg 2xl:text-xl fxl:text-2xl 3xl:text-3xl   mb-8 text-white">
+          <p className="text-base sm:text-lg 2xl:text-xl fxl:text-2xl 3xl:text-3xl   mb-8 ">
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci
             eligendi, aspernatur aperiam itaque perspiciatis vero consectetur
             rerum quae, illum ducimus quos ipsum placeat saepe illo laboriosam
             modi laborum nobis facere.{" "}
           </p>
-
-          <Link href="/blog">
-            <Cta>Leggi</Cta>
-          </Link>
         </div>
-        {/* <div className="flex">{jsxPosts}</div> */}
+        <div className="flex w-full flex-col lg:flex-row">{jsxPosts}</div>
+        <Link href="/blog">
+          <div>
+            <Cta>Leggi</Cta>
+          </div>
+        </Link>
       </div>
     </div>
   );
--- components/blogSection/postSection.js
+++ components/blogSection/postSection.js
@@ -1,3 +1,4 @@
+import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 //to use Image with an external url, add some config on next.config.js
@@ -7,11 +8,11 @@ import Siena from "../../public/assets/locationTour/siena/siena2.jpg";
 import { getDate } from "../../utils/utils";
 import Cta from "../button/button";
 
-export default function Post({ post, featuredMedia }) {
+export default function PostSection({ post, featuredMedia }) {
   console.log(post);
   return (
-    <div className="card w-full lg:w-96  shadow-xl">
-      <Link href={`/posts/${post?.slug}`}>
+    <div className="card w-full lg:w-50 lg:h-[20rem]  shadow-xl mb-6 lg:mb-2 mr-4 bg-white text-black ">
+      {/* <Link href={`/posts/${post?.slug}`}>
         <figure>
           <Image
             src={
@@ -21,28 +22,31 @@ export default function Post({ post, featuredMedia }) {
             width={1000}
             height={1000}
             alt={featuredMedia?.["alt_text"] || "LouOnTour image"}
-            className="w-full h-[350px] object-cover rounded-t-lg"
+            className="w-full h-[250px] object-cover rounded-t-lg"
             priority
           />
         </figure>
-      </Link>
+      </Link> */}
 
-      <div className="card-body">
-        <div className="py-2">
-          <small className="badge badge-info badge-lg text-white">
+      <div className="card-body !p-4">
+        <div>
+          <small className="badge badge-info badge-lg text-white text-xs">
             {getDate(post?.date)}
           </small>
         </div>
         <Link href={`/posts/${post?.slug}`}>
           <h5
-            className="card-title text-black pb-2 hover:underline"
+            className="card-title text-black pb-2 hover:underline text-base"
             dangerouslySetInnerHTML={{ __html: post?.title?.rendered }}
           ></h5>
         </Link>
-        <p dangerouslySetInnerHTML={{ __html: post?.excerpt?.rendered }}></p>
+        <p
+          dangerouslySetInnerHTML={{ __html: post?.excerpt?.rendered }}
+          className="text-sm"
+        ></p>
         <div className="card-actions justify-end">
-          <Link href={`/posts/${post?.slug}`}>
-            <Cta>See more</Cta>
+          <Link href={`/posts/${post?.slug}`} className="link">
+            See more
           </Link>
         </div>
       </div>
