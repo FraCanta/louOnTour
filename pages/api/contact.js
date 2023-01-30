8742e354d233c7b505f6716e4b3f97c507f3450f
FraCanta <you@example.comfcantale14@gmail.com>
2023-01-30 15:11:38 +0100

    contact form

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -3,25 +3,19 @@ import nodemailer from "nodemailer";
 export default async function mailer(req, res) {
   const { name, email, message } = req.body;
   const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
+    host: "smtp.louontour.it",
+    port: 25,
+    secure: false,
     auth: {
-      user: "fcantale14@gmail.com",
-      pass: "wtpyvhzvqsecycqt",
-      // user: "fcantale14@gmail.com",
-      // pass: "sarimale84",
+      user: "info@louontour.it",
+      pass: "Mailweb.lq_79!",
     },
   });
 
   try {
     await transporter.sendMail({
-      from: "fcantale14@gmail.com",
-      to: [
-        "eddironz@hotmail.com",
-        "fcantale14@gmail.com",
-        "quaglialuisa@gmail.com",
-      ],
+      from: "info@louontour.it",
+      to: ["fcantale14@gmail.com", "luisaquaglia.tourguide@gmail.com"],
       subject: `You've got a LouMessage da ${name}  `,
       html: `<p>Hai una richiesta di informazioni da ${name} </p><br>
         <p><strong>Email: </strong> ${email}</p><br>
