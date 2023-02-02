90b72743d2ea586108dd0ea818a84f09a2cf6c4f
FraCanta <you@example.comfcantale14@gmail.com>
2023-02-02 15:20:51 +0100

    og img

--- pages/_document.js
+++ pages/_document.js
@@ -40,7 +40,7 @@ export default function Document() {
         <meta name="twitter:description" content="Guida Turistica" />
         <meta
           name="twitter:image"
-          content="https://louontour.it/assets/preview-05.png"
+          content="https://louontour.it/assets/og-img.png"
         />
       </Head>
 
