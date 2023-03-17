11eadcd9ceb6696db13cad850f738f0f3d47d64d
FraCanta <you@example.comfcantale14@gmail.com>
2023-03-17 14:42:55 +0100

    email template

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -15,11 +15,39 @@ export default async function mailer(req, res) {
   try {
     await transporter.sendMail({
       from: "info@louontour.it",
-      to: ["fcantale14@gmail.com", "luisaquaglia.tourguide@gmail.com"],
+      to: ["fcantale14@gmail.com"],
       subject: `You've got a LouMessage da ${name}  `,
-      html: `<p>Hai una richiesta di informazioni da ${name} </p><br>
-        <p><strong>Email: </strong> ${email}</p><br>
-        <p><strong>Messaggio: </strong> ${message}</p><br>
+      // html: `<p>Hai una richiesta di informazioni da ${name} </p><br>
+      //   <p><strong>Email: </strong> ${email}</p><br>
+      //   <p><strong>Messaggio: </strong> ${message}</p><br>
+      // "luisaquaglia.tourguide@gmail.com"
+      // `,
+      html: ` <div></div><div style="background: linear-gradient(
+    90deg,
+    hsla(204, 68%, 41%, 1) 0%,
+    hsla(205, 100%, 67%, 1) 100%
+  ); height: 10px; width:100%">
+    </div>
+   <div><h4 style="font-size: 22px; text-decoration: underline; font-family: 'Noto Sans', sans-serif;">Hai una richiesta di informazioni da ${name} </h4></div>
+<div >
+<h4 style="font-size: 20px; font-family: 'Noto Sans', sans-serif;">Sono ${name} ,
+</h4>
+<p style="font-size: 16px; font-family: 'Noto Sans', sans-serif;">${message}</p>
+<p style="font-size: 16px; font-family: 'Noto Sans', sans-serif;">Referenze del contatto: </p>
+
+<p>${email}</p>
+
+
+</div><div style="background: linear-gradient(
+    90deg,
+    hsla(204, 68%, 41%, 1) 0%,
+    hsla(205, 100%, 67%, 1) 100%
+  ); height: 10px; width:100%">
+</div>
+‍
+
+
+    </div>
       `,
     });
   } catch (error) {
