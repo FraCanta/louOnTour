a0f34f863535096aecb574f0206ed04c1c0a4e0c
FraCanta <you@example.comfcantale14@gmail.com>
2023-06-30 10:04:43 +0200

    button

--- components/button/button.js
+++ components/button/button.js
@@ -3,9 +3,9 @@ import { Icon } from "@iconify/react";
 
 function Cta({ children }) {
   return (
-    <button className="flex items-center justify-between px-4 w-[9.875rem] h-[3.375rem] rounded-lg relative text-white  font-[600] shadow-md border border-2  bg-[#FE6847] border-[#FE6847] ring-2 ring-[#FE6847] hover:scale-110 uppercase font-extrabold grain">
+    <div className="flex items-center justify-between px-4 w-[9.875rem] h-[3.375rem] rounded-lg relative text-white  font-[600] shadow-md border border-2  bg-[#FE6847] border-[#FE6847] ring-2 ring-[#FE6847] hover:scale-110 uppercase font-extrabold grain">
       {children} <Icon icon="bi:arrow-right" />
-    </button>
+    </div>
   );
 }
 
