f5f54f9d5f137aac75c0691ff1670e0626731c87
FraCanta <fcantale14@gmail.com>
2025-05-30 13:58:19 +0200

    redirects

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -1,110 +1,104 @@
-//pages/sitemap.xml.js
 const headlessSite = "https://luisaquaglia-tourguide.com";
 import LayoutTranslation from "../public/locales/layout.json";
-
-import {
-  getPosts,
-  getTagId,
-  getPostsByLanguageAndBlogOwner,
-} from "../utils/wordpress";
+import { getPostsByLanguageAndBlogOwner } from "../utils/wordpress";
 
 function generateSiteMap(posts) {
   return `<?xml version="1.0" encoding="UTF-8"?>
-     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">       
-       <url>
-         <loc>https://luisaquaglia-tourguide.com/</loc>
-       </url>
-       <url>
-         <loc>https://luisaquaglia-tourguide.com/en</loc>
-       </url>
-       <url>
-         <loc>https://luisaquaglia-tourguide.com/blog</loc>
-       </url>
-       <url>
-         <loc>https://luisaquaglia-tourguide.com/en/blog</loc>
-       </url>
-         <url>
-         <loc>https://luisaquaglia-tourguide.com/chi-sono</loc>
-       </url>
-        <url>
-         <loc>https://luisaquaglia-tourguide.com/en/chi-sono</loc>
-       </url>
-         <url>
-         <loc>https://luisaquaglia-tourguide.com/contatti</loc>
-       </url>
-         <url>
-         <loc>https://luisaquaglia-tourguide.com/en/contatti</loc>
-       </url>
-       <url>
-       <loc>https://luisaquaglia-tourguide.com/tours-da-fare</loc>
-     </url>
-       <url>
-       <loc>https://luisaquaglia-tourguide.com/en/tours-da-fare</loc>
-     </url>
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 
-${LayoutTranslation?.menu?.it?.map?.markers
-  .map((el, i) => {
-    return `
-            <url>
-             <loc>${`${headlessSite}/locations/${el?.title}`}</loc>
-            </url>
-           `;
-  })
-  .join("")}
+    <url>
+      <loc>${headlessSite}/</loc>
+    </url>
+    <url>
+      <loc>${headlessSite}/en</loc>
+    </url>
+    <url>
+      <loc>${headlessSite}/blog</loc>
+    </url>
+    <url>
+      <loc>${headlessSite}/en/blog</loc>
+    </url>
+    <url>
+      <loc>${headlessSite}/chi-sono</loc>
+    </url>
+    <url>
+      <loc>${headlessSite}/en/chi-sono</loc>
+    </url>
+    <url>
+      <loc>${headlessSite}/contatti</loc>
+    </url>
+    <url>
+      <loc>${headlessSite}/en/contatti</loc>
+    </url>
+    <url>
+      <loc>${headlessSite}/tours-da-fare</loc>
+    </url>
+    <url>
+      <loc>${headlessSite}/en/tours-da-fare</loc>
+    </url>
 
-  ${LayoutTranslation?.menu?.en?.map?.markers
-    .map((el, i) => {
-      return `
-            <url>
-             <loc>${`${headlessSite}/en/locations/${el?.title}`}</loc>
-         </url>
-           `;
-    })
-    .join("")}
-
-       
-    ${posts?.it
-      .map(({ id, slug, tags, date }) => {
-        const receivedDate = new Date(date);
-        const isoDate = receivedDate.toISOString();
+    ${LayoutTranslation?.menu?.it?.map?.markers
+      .map((el) => {
         return `
-      <url>
-          <loc>${`${headlessSite}/posts/${slug}`}</loc>
-          <lastmod>${`${isoDate}`}</lastmod>
-          <changefreq>weekly</changefreq>
-          <priority>0.5</priority>
-      </url>
-    `;
+          <url>
+            <loc>${`${headlessSite}/locations/${encodeURIComponent(
+              el?.title
+            )}`}</loc>
+          </url>`;
       })
       .join("")}
-      ${posts?.en
-        .map(({ id, slug, tags, date }) => {
-          const receivedDate = new Date(date);
-          const isoDate = receivedDate.toISOString();
-          return `
-       <url>
-           <loc>${`${headlessSite}/en/posts/${slug}`}</loc>
-           <lastmod>${`${isoDate}`}</lastmod>
-           <changefreq>weekly</changefreq>
-           <priority>0.5</priority>
-       </url>
-     `;
-        })
-        .join("")}
-     </urlset>
-   `;
+
+    ${LayoutTranslation?.menu?.en?.map?.markers
+      .map((el) => {
+        return `
+          <url>
+            <loc>${`${headlessSite}/en/locations/${encodeURIComponent(
+              el?.title
+            )}`}</loc>
+          </url>`;
+      })
+      .join("")}
+
+    ${posts?.it
+      .map(({ slug, date }) => {
+        const isoDate = new Date(date).toISOString();
+        return `
+          <url>
+            <loc>${`${headlessSite}/posts/${slug}`}</loc>
+            <lastmod>${isoDate}</lastmod>
+            <changefreq>weekly</changefreq>
+            <priority>0.5</priority>
+          </url>`;
+      })
+      .join("")}
+
+    ${posts?.en
+      .map(({ slug, date }) => {
+        const isoDate = new Date(date).toISOString();
+        return `
+          <url>
+            <loc>${`${headlessSite}/en/posts/${slug}`}</loc>
+            <lastmod>${isoDate}</lastmod>
+            <changefreq>weekly</changefreq>
+            <priority>0.5</priority>
+          </url>`;
+      })
+      .join("")}
+
+  </urlset>`;
 }
 
-function SiteMap() {
-  // getServerSideProps will do the heavy lifting
+// Optional: sitemap page component (invisibile)
+export default function SiteMap() {
+  return null;
 }
 
+// Generate sitemap at runtime (SSR)
 export async function getServerSideProps({ res }) {
-  const resObj = await getPostsByLanguageAndBlogOwner();
-  const sitemap = generateSiteMap(resObj);
-  console.log(resObj);
+  const posts = await getPostsByLanguageAndBlogOwner();
+  const sitemap = generateSiteMap(posts);
+
   res.setHeader("Content-Type", "text/xml");
-  // we send the XML to the browser
   res.write(sitemap);
   res.end();
 
@@ -112,5 +106,3 @@ export async function getServerSideProps({ res }) {
     props: {},
   };
 }
-
-export default SiteMap;
