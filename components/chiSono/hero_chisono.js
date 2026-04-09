37e15ee11351ed6e08c50ef3a96f6c0e8892640b
FraCanta <fcantale14@gmail.com>
2026-04-09 17:12:38 +0200

    aggiornamento

--- components/chiSono/hero_chisono.js
+++ components/chiSono/hero_chisono.js
@@ -43,7 +43,7 @@ function HeroChiSono({ translation }) {
         </motion.div>
 
         <MaskText>
-          <h2 className="text-[4rem] xs:text-[5rem] lg:text-[13rem] fxl:text-[17rem] font-bold leading-none py-1.5">
+          <h2 className="text-[4rem] xs:text-[5rem] lg:text-[10.5rem] 2xl:text-[13rem] fxl:text-[17rem] font-bold leading-none py-1.5">
             {translation?.title?.primo}{" "}
           </h2>
         </MaskText>
@@ -51,11 +51,11 @@ function HeroChiSono({ translation }) {
 
       <motion.div
         variants={item}
-        className="grid grid-cols-1 lg:grid-cols-2 -mt-[6%] 2xl:-mt-[10%] fxl:-mt-[10%] gap-4 lg:w-3/5  lg:gap-10"
+        className="grid grid-cols-1 lg:grid-cols-2 -mt-[6%] lg:-mt-[12%] 2xl:-mt-[10%] fxl:-mt-[10%] gap-4 lg:w-3/5  lg:gap-10"
       >
-        <div className="flex flex-col justify-between gap-4 lg:px-2 lg:mt-6 fxl:mt-14">
+        <div className="flex flex-col justify-between gap-4 lg:px-2 xl:mt-10 2xl:mt-6 fxl:mt-14">
           <MaskText>
-            <p className="text-[#c9573c]/80 w-full text-lg lg:text-[1.5rem] fxl:text-3xl 3xl:text-[2.5rem] leading-none 3xl:leading-[3.5rem] mb-5 font-normal">
+            <p className="text-[#c9573c]/80 w-full text-lg 2xl:text-[1.5rem] fxl:text-3xl 3xl:text-[2.5rem] leading-none 3xl:leading-[3.5rem] 2xl:mb-5 font-normal">
               {translation?.title?.secondo}
               <br />
               {translation?.title?.terzo}{" "}
@@ -64,14 +64,14 @@ function HeroChiSono({ translation }) {
           <div className="flex flex-col gap-4">
             <motion.div
               variants={item}
-              className="flex flex-col items-center w-full gap-3 lg:gap-4 lg:flex-row"
+              className="flex flex-col items-center w-full gap-3 2xl:gap-4 lg:flex-row"
             >
               <p className="text-para">{translation?.paragraph}</p>
             </motion.div>
 
             <motion.div
               variants={item}
-              className="flex flex-col items-center w-full gap-3 lg:gap-4 lg:flex-row"
+              className="flex flex-col items-center w-full gap-3 2xl:gap-4 lg:flex-row"
             >
               <div className="flex justify-center w-full lg:max-w-max">
                 <CtaPrimary
@@ -85,7 +85,7 @@ function HeroChiSono({ translation }) {
           </div>
         </div>
 
-        <div className="relative aspect-square h-[300px] w-full lg:h-full lg:w-full fxl:h-[500px] fxl:w-[500px] rounded-sm shadow-2xl  ">
+        <div className="relative aspect-square h-[300px] w-full xl:h-full 2xl:w-full fxl:h-[500px] fxl:w-[500px] rounded-sm shadow-2xl  ">
           <Image
             src={LouAvatar}
             alt="lou_avatar"
