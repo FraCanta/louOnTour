31098cc59bc916ed02519d8c9511b1bb3415f526
FraCanta <fcantale14@gmail.com>
2026-04-13 19:53:01 +0200

    accessibilità

--- pages/_app.js
+++ pages/_app.js
@@ -33,7 +33,12 @@ function MyApp({ Component, pageProps, router }) {
       <DynamicLayout>
         <Component {...pageProps} />
       </DynamicLayout>
-
+      {/* <!-- Elfsight Accessibility | Untitled Accessibility --> */}
+      <Script src="https://elfsightcdn.com/platform.js" async></Script>
+      <div
+        className="elfsight-app-acbdfc91-1069-4174-a7aa-9fd11406bb2e"
+        data-elfsight-app-lazy
+      ></div>
       {/* Global site tag (gtag.js) - Google Analytics */}
       <Script
         type="text/javascript"
